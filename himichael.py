4858e73597288c6acbdfb880580dd427941d0b51
jonahbaayen <jonahbaayen@outlook.com>
2023-01-30 22:50:13 -0500

    my dick in yo mouf

---
+++ himichael.py
@@ -0,0 +1,30 @@
+def main():
+    print(suckcock())
+
+    my_dick = penis(10)
+    print(my_dick.get_cock())
+
+def suckcock():
+    return 'cum'
+
+class penis:
+    def __init__(self, length):
+        self.length = length
+        self.hairy = True
+
+    def shave(self):
+        self.hairy = False
+
+    def bush(self):
+        self.hairy = True
+
+    def get_cock(self):
+        dick = '8'
+        for i in range(self.length):
+            dick += '='
+        dick += 'D'
+
+        return dick
+
+if __name__ == '__main__':
+    main()
